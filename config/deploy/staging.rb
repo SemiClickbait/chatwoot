1887935a41f455e597a8a657fb9ab8983dea19a9
Bandwidth <jadonphillips@icloud.com>
2019-12-08 19:39:07 -0500

    Update staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options = config.action_mailer.default_url_options = { :host => 'anglechat.herokuapp.com', :protocol => 'https' }
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
